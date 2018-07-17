2a3a5cc281cd5cde957e7a3d6773fb8692e3c796
Yujia Li <yli302@ucsc.edu>
2018-07-17 14:22:43 -0700

    update layouts

--- SmartAccount2/app/src/main/java/com/smartaccount/smartaccount/MainActivity.java
+++ SmartAccount2/app/src/main/java/com/smartaccount/smartaccount/MainActivity.java
@@ -1,15 +1,51 @@
 package com.smartaccount.smartaccount;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        loadFragment(new HomeFragment());
+    }
 
+    private boolean loadFragment(Fragment fragment){
+        if (fragment!=null){
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment= new HomeFragment();
+                break;
+            case R.id.navigation_dashboard:
+                fragment = new CalendarFragment();
+                break;
+            case R.id.navigation_notifications:
+                fragment = new AnalysisFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
