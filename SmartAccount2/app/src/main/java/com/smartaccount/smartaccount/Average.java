c430f11a66e90fa0558f957bd313413102e266f7
何清源 <heqingyuan@heqingyuandeMacBook.local>
2018-07-17 20:12:36 -0700

    added average.java

---
+++ SmartAccount2/app/src/main/java/com/smartaccount/smartaccount/Average.java
@@ -0,0 +1,34 @@
+package com.smartaccount.smartaccount;
+
+public class Average {
+    public static void main(String[] args) {
+
+    }
+
+    //sum of the expend of a particular day
+    static double sotd()
+
+    {
+        //calculate the sum
+        double sum = 1;//sum of all the expend of the day
+        return sum;
+    }
+
+    static double aotw()
+
+    {
+        double sum = 7;//sum of the sum of previous 7 days
+        return sum/7.0;
+
+    }
+
+    static double aotm()
+
+    {
+        double sum = 30;//sum of the sum of previous 30 days
+        return sum/30.0;
+
+    }
+
+
+}
