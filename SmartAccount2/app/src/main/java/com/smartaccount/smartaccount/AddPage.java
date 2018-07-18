5f8056fda9d8eb444a7a9cc79197a0345af8a1bb
Yujia Li <yli302@ucsc.edu>
2018-07-18 02:34:44 -0700

    add page can submit info. to firebase

--- SmartAccount2/app/src/main/java/com/smartaccount/smartaccount/AddPage.java
+++ SmartAccount2/app/src/main/java/com/smartaccount/smartaccount/AddPage.java
@@ -7,8 +7,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
@@ -18,24 +23,62 @@ public class AddPage extends AppCompatActivity{
     private TextView dateView;
     private int year, month, day;
 
+    private int amount;
+    private String category;
+    private String currency;
+    private String note;
+    private String addType;
+
+    DatabaseReference myRef;
+
+    EditText editAmount;
+    EditText editCategory;
+    EditText editNote;
+    ToggleButton editType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        //initViews
+        editAmount = (EditText) findViewById (R.id.addRecordAmount);
+        editCategory = (EditText) findViewById (R.id.addRecordCategory);
+        editNote = (EditText) findViewById (R.id.addRecordNote);
+        editType = (ToggleButton) findViewById(R.id.addRecordIncomeOutcomeButton);
+
+        // Write a message to the database
+        myRef =  FirebaseDatabase.getInstance().getReference("user");
+
         dateView = (TextView) findViewById(R.id.currentDate);
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
 
-        month = calendar.get(Calendar.MONTH);
+        month = calendar.get(Calendar.MONTH) + 1;
         day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month+1, day);
+        showDate(year, month, day);
     }
 
     public void jump_MainPage(View view){
+        startActivity(new Intent(AddPage.this,MainActivity.class));
+    }
+
+    //submit and return to main page after pushing add button.
+    public void addSubmit(View view){
+        amount = Integer.parseInt(editAmount.getText().toString());
+        category = editCategory.getText().toString();
+        currency = "USD";
+        note = editNote.getText().toString();
+        if(editType.isChecked() == false) {
+            addType = "Income";
+        } else {
+            addType = "Outcome";
+        }
+
+        History newHist = new History(amount, year, month, day, category, currency, note, addType);
+        myRef.child("history1").setValue(newHist);
 
         startActivity(new Intent(AddPage.this,MainActivity.class));
-
     }
 
     @SuppressWarnings("deprecation")
@@ -70,6 +113,9 @@ public class AddPage extends AppCompatActivity{
             };
 
     private void showDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
         dateView.setText(new StringBuilder().append(day).append("/")
                 .append(month).append("/").append(year));
     }
